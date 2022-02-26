0e822e0e7b3af32e23e80844411bd006d7d26a1d
Luke Burke <lukejburke@live.co.uk>
2022-02-26 04:04:46 +0000

    Worked on adding folder support

--- NewTab.Web/Models/Bookmark.cs
+++ NewTab.Web/Models/Bookmark.cs
@@ -4,27 +4,47 @@ namespace NewTab.Web.Models;
 
 public class Bookmark
 {
-	[Json("children")]
-	public Bookmark[]? Children { get; set; }
+    [Json("children")]
+    public List<Bookmark>? Children { get; set; }
 
-	[Json("date_added")]
-	public string? DateAdded { get; set; }
+    [Json("date_added")]
+    public string? DateAdded { get; set; }
 
-	[Json("date_modified")]
-	public string? DateModified { get; set; }
+    [Json("date_modified")]
+    public string? DateModified { get; set; }
 
-	[Json("guid")]
-	public Guid? Guid { get; set; }
+    [Json("guid")]
+    public Guid? Guid { get; set; }
 
-	[Json("id")]
-	public string? Id { get; set; }
+    [Json("id")]
+    public string? Id { get; set; }
 
-	[Json("name")]
-	public string? Name { get; set; }
+    [Json("name")]
+    public string? Name { get; set; }
 
-	[Json("type")]
-	public string? Type { get; set; }
+    [Json("type")]
+    public string? Type { get; set; }
 
-	[Json("url")]
-	public string? Url { get; set; }
+    [Json("url")]
+    public string? Url { get; set; }
+
+
+    public Bookmark() { }
+    public Bookmark(Bookmark? bookmark)
+    {
+        if (bookmark == null) return;
+
+        if (bookmark.Children != null) {
+            Children = new();
+            Children.AddRange(bookmark.Children);
+        }
+
+        DateAdded = bookmark.DateAdded;
+        DateModified = bookmark.DateModified;
+        Guid = bookmark.Guid;
+        Id = bookmark.Id;
+        Name = bookmark.Name;
+        Type = bookmark.Type;
+        Url = bookmark.Url;
+    }
 }
